b66a397b8cb4f4c21907b7e8bb7b66c37c23236e
Gabriel Antony <gmamanican@unsa.edu.pe>
2024-12-08 23:03:43 -0500

    refactor: delete unnecessary attribute

--- src/components/HomeItem.tsx
+++ src/components/HomeItem.tsx
@@ -1,24 +1,27 @@
 import Image from "next/image";
+import Link from "next/link";
 
 interface Params {
   title: string;
   imageUrl: string;
+  path: string;
 }
 
-export const HomeItem = ({ title, imageUrl }: Params) => {
+export const HomeItem = ({ title, imageUrl, path }: Params) => {
   return (
-    <div className="w-80 h-24 bg-[#003366] rounded-[14px] m-6 flex flex-row justify-center p-4 items-center">
-      <Image
-        className="object-contain "
-        src={imageUrl}
-        alt={title}
-        layout="intrinsic" // Esto ajusta la imagen a sus dimensiones originales
-        width={63}
-        height={63}
-      />
-      <span className="text-white text-lg w-3/5 p-2 pl-10 leading-6 text-left">
-        {title}
-      </span>
-    </div>
+    <Link href={path}>
+      <div className="w-80 h-24 bg-[#003366] rounded-[14px] m-6 flex flex-row justify-center p-4 items-center">
+        <Image
+          className="object-contain "
+          src={imageUrl}
+          alt={title}
+          width={63}
+          height={63}
+        />
+        <span className="text-white text-lg w-3/5 p-2 pl-10 leading-6 text-left">
+          {title}
+        </span>
+      </div>
+    </Link>
   );
 };
