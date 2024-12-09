bceb9858be08fb116b162be7ef8919051e710e2e
Gabriel Antony <gmamanican@unsa.edu.pe>
2024-12-08 23:02:58 -0500

    feature: add paths to items from home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -6,23 +6,26 @@ const listItems = [
     title: "Fichas de inscripción",
     imageUrl:
       "https://raw.githubusercontent.com/Gabicho258/Proyecto-M-Business/refs/heads/master/src/assets/ficha.png",
+    path: "/fichas",
   },
   {
     title: "Socios",
     imageUrl:
       "https://raw.githubusercontent.com/Gabicho258/Proyecto-M-Business/refs/heads/master/src/assets/socio.png",
+    path: "/socios",
   },
   {
     title: "Deportes",
     imageUrl:
       "https://raw.githubusercontent.com/Gabicho258/Proyecto-M-Business/refs/heads/master/src/assets/deporte.png",
+    path: "/deportes",
   },
 ];
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center">
-      <h1 className="text-xl w-60 text-center mt-20 mb-6 ">
+      <h1 className="text-xl w-60 text-center mt-20 mb-6 font-semibold">
         Administración de inscripciones de deportes
       </h1>
       <div>
