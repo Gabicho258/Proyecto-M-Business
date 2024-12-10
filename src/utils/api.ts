8d32b2c013bad6c8e4f63cb58a1a7e3c9f7f3432
Gabriel Antony <gmamanican@unsa.edu.pe>
2024-12-10 13:24:08 -0500

    Test for deploy

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,6 @@
 import { Deporte, FichaInscripcion, Socio } from "@prisma/client";
 
-const API_URL = "http://localhost:3000";
+const API_URL = process.env.NEXT_PUBLIC_VERCEL_URL || "http://localhost:3000";
 
 // Socios Endpoints
 
